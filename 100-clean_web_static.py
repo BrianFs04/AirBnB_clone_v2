6929c489e89a3f0d70275bea4a821317acae068c
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-09 03:45:12 +0000

    [Feature]: Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that generates a .tgz archive
+from the contents of the web_static"""
+from fabric.api import *
+
+env.hosts = ['34.74.149.164', '3.93.169.162']
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/holberton"
+
+
+def do_clean(number=0):
+    '''Deletes out-of-date archives'''
+    number = int(number)
+
+    if number == 0 or number == 1:
+        local('cd versions; ls | head -n -1 | xargs rm -rf')
+        run('cd /data/web_static/releases ; ls | head -n -1 | xargs rm -rf')
+    elif number == 2:
+        local('cd versions; ls | head -n -2 | xargs rm -rf')
+        run('cd /data/web_static/releases ; ls | head -n -2 | xargs rm -rf')
