9ca5e05f82d9db28ec0cd0bb74e2dbe4cde0992f
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-10 19:33:57 +0000

    [Fix]: Typo

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,6 @@ from fabric.api import *
 
 env.hosts = ['34.74.149.164', '3.93.169.162']
 env.user = "ubuntu"
-env.key_filename = "~/.ssh/holberton"
 
 
 def do_clean(number=0):
@@ -16,6 +15,6 @@ def do_clean(number=0):
         local('cd versions; ls | head -n -1 | xargs rm -rf')
         run('cd /data/web_static/releases ; ls | head -n -1 | xargs rm -rf')
     else:
-        local('cd versions; ls | head -n {} | xargs rm -rf'.format(number))
+        local('cd versions; ls | head -n -{} | xargs rm -rf'.format(number))
         run('cd /data/web_static/releases ; ls | head -n -{} | xargs rm -rf'.
             format(number))
