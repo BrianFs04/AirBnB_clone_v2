83b1928e8254c146b384898654dc90e3d561a72c
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-10 18:58:09 +0000

    [Fix]: Condition to adding more numbers

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -15,6 +15,7 @@ def do_clean(number=0):
     if number == 0 or number == 1:
         local('cd versions; ls | head -n -1 | xargs rm -rf')
         run('cd /data/web_static/releases ; ls | head -n -1 | xargs rm -rf')
-    elif number == 2:
-        local('cd versions; ls | head -n -2 | xargs rm -rf')
-        run('cd /data/web_static/releases ; ls | head -n -2 | xargs rm -rf')
+    else:
+        local('cd versions; ls | head -n {} | xargs rm -rf'.format(number))
+        run('cd /data/web_static/releases ; ls | head -n {} | xargs rm -rf'.
+            format(number))
