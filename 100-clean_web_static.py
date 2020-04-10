2159cce254867ee98b1c7f221a11efe0c6ff5710
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-10 19:39:19 +0000

    [Fix]: Typo

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,12 +9,13 @@ env.user = "ubuntu"
 
 def do_clean(number=0):
     '''Deletes out-of-date archives'''
+
     number = int(number)
 
-    if number == 0 or number == 1:
+    if number == 1 or number == 0:
         local('cd versions; ls | head -n -1 | xargs rm -rf')
-        run('cd /data/web_static/releases ; ls | head -n -1 | xargs rm -rf')
+        run('cd /data/web_static/releases; ls | head -n -1 | xargs rm -rf')
     else:
         local('cd versions; ls | head -n -{} | xargs rm -rf'.format(number))
-        run('cd /data/web_static/releases ; ls | head -n -{} | xargs rm -rf'.
+        run('cd /data/web_static/releases; ls | head -n -{} | xargs rm -rf'.
             format(number))
