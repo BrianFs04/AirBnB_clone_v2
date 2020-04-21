fd3b0afcb5bca75a138efbf974c27068ce29c650
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-21 20:06:40 +0000

    [Feature]: Route that lists states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Flask web application"""
+from flask import Flask, escape, render_template
+from models import storage
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def closes(error):
+    """Closes the session"""
+    storage.close()
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    """Lists cities by states"""
+
+    states = storage.all('State')
+
+    return render_template('8-cities_by_states.html', states=states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
