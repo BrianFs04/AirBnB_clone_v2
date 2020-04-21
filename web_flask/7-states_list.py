3e3eecdfe72ca925e94d418ecac2a39ce44b2159
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-21 20:23:16 +0000

    [Feature]: Route that lists states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Flask web application"""
+from flask import Flask, escape, render_template
+from models import storage
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def closes(error):
+    """Closes the session"""
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """Lists states"""
+
+    states = storage.all('State')
+
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
