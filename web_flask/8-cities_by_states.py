3b8cb603be0e9976d693a0639da6c9e4582f033a
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-21 20:48:44 +0000

    [Fix]: Typo

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,7 @@ def closes(error):
 
 @app.route('/cities_by_states')
 def cities_by_states():
-    """Lists cities by their correspoding states"""
+    """Lists cities by their corresponding state"""
 
     states = storage.all('State')
 
