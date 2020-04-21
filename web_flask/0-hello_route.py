7a82dafbbc53811e431885ceba2e368cb2da3570
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-04-21 19:24:42 +0000

    [Fix]: Setting strict slashes globally

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,10 @@
 """Flask web application"""
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Displays Hello HBNB!"""
     return "Hello HBNB!"
